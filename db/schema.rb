440ab9713a693d9740e8c444fcc2d111df400d3b
Khushboo Chandwani <khushboo.chandwani@appfolio.com>
2020-07-27 16:42:33 -0400

    form + saving image link + validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_07_24_211406) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "link"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
