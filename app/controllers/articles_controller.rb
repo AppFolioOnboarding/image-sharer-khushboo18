7ad4c17e0839b11e93efc5215f7aaba76fc87c84
Khushboo Chandwani <khushboo.chandwani@appfolio.com>
2020-07-27 16:42:48 -0400

    form + saving image link + validation

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+    def new
+        @article = Article.new
+        render :new
+    end
+
+    def index
+        @articles = Article.ArticlesController
+        render :index
+    end
+
+    def show
+        @article = Article.find(params[:id])
+        render :show
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to @article
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:link, :text)
+    end
+end
