13598e21ed3c5b543a6c1ba9244d2305be52cc91
Khushboo Chandwani <khushboo.chandwani@appfolio.com>
2020-07-27 18:03:22 -0400

    rubocop conventions

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,12 +6,14 @@ class Article < ApplicationRecord
 
   def valid_url
     return unless !link.start_with?('https://') && !link.start_with?('http://')
+    
     errors.clear
     errors.add(:link, ' is not a valid url')
   end
 
   def valid_image_url
     return unless !link.include?('.jpg') && !link.include?('.png') && !link.include?('.jpeg')
+
     errors.clear
     errors.add(:link, ' is not a valid image url')
   end
@@ -21,7 +23,8 @@ class Article < ApplicationRecord
     req = Net::HTTP.new(url.host, url.port)
     req.use_ssl = true if url.scheme == 'https'
     res = req.request_head(url.path)
-    return unless res.code != 200 
+    return unless res.code != "200"
+
     errors.clear
     errors.add(:link, ' is not a valid url')
   end
