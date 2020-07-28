d526b45cc0d0681eecb42d144acdb460fad86649
Khushboo Chandwani <khushboo.chandwani@appfolio.com>
2020-07-28 16:34:10 -0400

    model test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'article count' do
+    assert_equal Article.count, 2
+  end
 end
