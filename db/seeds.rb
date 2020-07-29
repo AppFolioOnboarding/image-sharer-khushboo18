575143ead7d32aa6f4d296659f092d3daaec03b3
Khushboo Chandwani <khushboo.chandwani@appfolio.com>
2020-07-29 13:27:51 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create!([
+  { text: 'Image 1', link: 'https://www.allaboutbirds.org/guide/assets/photo/67385731-480px.jpg' },
+  { text: 'Image 2', link: 'https://www.sciencemag.org/sites/default/files/styles/article_main_large/'\
+  'public/butterfly_16x9_0.jpg?itok=jZ3DYvGK' },
+  { text: 'Image 3', link: 'https://wpcdn.us-east-1.vip.tn-cloud.net/www.channel3000.com/content/'\
+  'uploads/2020/06/S108767171-1024x576.jpg' },
+  { text: 'Image 4', link: 'https://dogtime.com/assets/uploads/2018/10/puppies-cover-1280x720.jpg' },
+  { text: 'Image 5', link: 'https://s.abcnews.com/images/US/'\
+  'cookie-monster-file-ap-jef-191104_hpMain_16x9_992.jpg' },
+  { text: 'Image 6', link: 'https://i.pinimg.com/originals/3c/8a/d8/3c8ad86ce17206b9613f52c6c09bde02.jpg' },
+  { text: 'Image 7', link: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/'\
+  'article_thumbnails/reference_guide/outdoor_cat_risks_ref_guide/1800x1200_outdoor_cat_risks_ref_guide.jpg' },
+  { text: 'Image 8', link: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/'\
+  '2019/5/17/0/WU2207_Mermaid-Ice-Cream_s4x3.jpg.rend.hgtvcom.616.462.suffix/1558112854371.jpeg' },
+  { text: 'Image 9', link: 'https://edsurge.imgix.net/uploads/post/image/12857/guild-1573610501.jpg?'\
+  'auto=compress%2Cformat&w=1024&h=512&fit=crop' },
+  { text: 'Image 10', link: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg' },
+  { text: 'Image 11', link: 'https://hackernoon.com/hn-images/1*jFyawcsqoYctkTuZg6wQ1A.jpeg' },
+  { text: 'Image 12', link: 'https://1.bp.blogspot.com/-RtweK4GoS18/VcgnwdG9kNI/AAAAAAAArGU/'\
+  '5mxfYR1GK1A/s1600/6962952-birds-love-wallpaper-737211.jpg' },
+  { text: 'Image 13', link: 'https://www.mandysam.com/img/random.jpg' },
+  { text: 'Image 14', link: 'https://www.omfif.org/wp-content/uploads/2020/03/random-shocks-newweb.jpg' },
+  { text: 'Image 15', link: 'https://images.theconversation.com/files/299379/original/file-20191030-'\
+  '154716-1wc4d64.jpg?ixlib=rb-1.1.0&rect=0%2C12%2C2048%2C1023&q=45&auto=format&w=1356&h=668&fit=crop' },
+  { text: 'Image 16', link: 'https://www.scitecheuropa.eu/wp-content/uploads/2019/10/Space-Race.jpg' },
+  { text: 'Image 17', link: 'https://www.memesmonkey.com/images/memesmonkey/18/'\
+  '18c300816f3fe23be42618b09f59d6c1.jpeg' },
+  { text: 'Image 18', link: 'https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2015/11/10/0/CI_Costa-Farms-Ballad-aster.jpg'\
+  '.rend.hgtvcom.966.644.suffix/1447169929799.jpeg' },
+  { text: 'Image 19', link: 'https://media.comicbook.com/2019/06/'\
+  'mike-wazowski-fanart-monsters-inc-1176973-1280x0.jpeg' },
+  { text: 'Image 20', link: 'https://i.ytimg.com/vi/Tq_MdFz7mlE/maxresdefault.jpg' }
+])
