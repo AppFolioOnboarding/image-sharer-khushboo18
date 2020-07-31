4fefcc732edba345d98e7f7f0a70ee3ee69531ac
Khushboo Chandwani <khushboo.chandwani@appfolio.com>
2020-07-31 15:25:13 -0400

    controller test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -28,4 +28,25 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to article_url(Article.last)
   end
+
+  test 'add tags to image' do
+    assert_difference('Article.count', 1) do
+      post articles_url, params: { article: { link: 'https://ichef.bbci.co.uk/news/1024/cpsprodpb/90B5/production/_104154073_gettyimages-154948449.jpg',
+                                              text: 'A new llama',
+                                              tag_list: 'llama, happy, cute' } }
+    end
+    assert_redirected_to article_url(Article.last)
+    assert_equal Article.last.tags.count, 3
+    follow_redirect!
+    assert_response :success
+  end
+
+  test 'unpermitted fields raise errors' do
+    ActionController::Parameters.action_on_unpermitted_parameters = :raise
+    assert_raise ActionController::UnpermittedParameters do
+      post articles_url, params: { article: { link: 'https://i.ytimg.com/vi/Tq_MdFz7mlE/maxresdefault.jpg',
+                                              text: 'A new llama',
+                                              newField: 'not permitted' } }
+    end
+  end
 end
